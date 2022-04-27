081fb62ea8455113e2ac3418cbeaa830e3ce7b73
Tim Vercammen <83355491+timvercammen@users.noreply.github.com>
2022-04-27 15:11:13 +0200

    filters met voorlopig icoontjes als test

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Header from './Header';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Categories from "./Categories";
 
 const Skeleton = ({ children }) => {
   return (
     <>
       <Header />
       <Navbar />
+      <Categories />
       {children}
       <Footer />
     </>
