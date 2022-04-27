b68c4f19cac2768746f37e7529c1fe54726870ab
Michiel Peeters <michiel.peeters@anvil.be>
2022-04-27 12:03:24 +0200

    react-router-dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header className="header container">
       <div className="header__items container__medium">
-        <h1 className="header__items__text">Veilingen van ons</h1>
+        <h1 className="header__items__text">
+          <span>OneClick</span> Pirate
+        </h1>
+        <div>
+          <Link to={"#"}>Login</Link>
+          <Link to={"#"}>register</Link>
+        </div>
       </div>
     </header>
   );
