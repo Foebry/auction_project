458855b1e723d361d1a078e89b0eff8c141ea8b4
Michiel Peeters <michiel.peeters@anvil.be>
2022-04-27 11:52:37 +0200

    Folder Structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="header container">
+      <div className="header__items container__medium">
+        <h1 className="header__items__text">Veilingen van ons</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
