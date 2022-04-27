bf409ebd02f592eb84edbf2f2769d824840ee869
Michiel Peeters <michiel.peeters@anvil.be>
2022-04-27 16:36:10 +0200

    Basic Header with starting styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,41 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+  const [loggedIn, setLoggedIn] = useState(true);
+
   return (
     <header className="header container">
       <div className="header__items container__medium">
         <h1 className="header__items__text">
           <span>OneClick</span> Pirate
         </h1>
-        <div>
-          <Link to={"#"}>Login</Link>
-          <Link to={"#"}>register</Link>
+        <div className="header__items__links">
+          {!loggedIn && (
+            <Link className="link" to={"#"}>
+              Login
+            </Link>
+          )}
+          {!loggedIn && (
+            <Link className="link" to={"#"}>
+              Register
+            </Link>
+          )}
+          {loggedIn && (
+            <p>
+              Welcome <span>NAME</span>
+            </p>
+          )}
+          {loggedIn && (
+            <Link className="link" to={"#"}>
+              My Products
+            </Link>
+          )}
+          {loggedIn && (
+            <Link className="link" to={"#"}>
+              Logout
+            </Link>
+          )}
         </div>
       </div>
     </header>
