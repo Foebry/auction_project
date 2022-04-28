7f004581bcd76514b1559fca4ef7f1ae777c5a0b
Foebry <rain_fabry@hotmail.com>
2022-04-28 09:36:52 +0200

    context

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,45 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
-  const [loggedIn, setLoggedIn] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(true);
+    const { userId, setUserId } = useContext(AppContext);
 
-  return (
-    <header className="header container">
-      <div className="header__items container__medium">
-        <h1 className="header__items__text">
-          <span>OneClick</span> Pirate
-        </h1>
-        <div className="header__items__links">
-          {!loggedIn && (
-            <>
-              <Link className="link" to={"#"}>
-                Login
-              </Link>
-              <Link className="link" to={"#"}>
-                Register
-              </Link>
-            </>
-          )}
-          {loggedIn && (
-            <>
-              <p>
-                Welcome <span>NAME</span>
-              </p>
-              <Link className="link" to={"#"}>
-                My Products
-              </Link>
-              <Link className="link" to={"#"}>
-                Logout
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header container">
+            <div className="header__items container__medium">
+                <h1 className="header__items__text">
+                    <span>OneClick</span> Pirate
+                </h1>
+                <div className="header__items__links">
+                    {!loggedIn && (
+                        <>
+                            <Link className="link" to={"#"}>
+                                Login
+                            </Link>
+                            <Link className="link" to={"#"}>
+                                Register
+                            </Link>
+                        </>
+                    )}
+                    {loggedIn && (
+                        <>
+                            <p>
+                                Welcome <span>NAME</span>
+                            </p>
+                            <Link className="link" to={"#"}>
+                                My Products
+                            </Link>
+                            <Link className="link" to={"#"}>
+                                Logout
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
