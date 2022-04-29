9fdf0025d7d791aa6c4cb59e89fc17f85fe5329a
Michiel Peeters <michiel.peeters@anvil.be>
2022-04-29 10:43:39 +0200

    timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 
 const Timer = () => {
-    const [time, setTime] = useState(20);
+    const endDate = new Date("April 29, 2022 12:00:00");
+    const [time, setTime] = useState(endDate / 1000);
+
+    let now = Date.now();
+    console.log(now);
 
     let h = Math.floor(time / 3600);
     let m = Math.floor((time % 3600) / 60);
@@ -9,7 +13,7 @@ const Timer = () => {
 
     useEffect(() => {
         const timerId = setInterval(() => {
-            setTime((prevCounter) => prevCounter - 1);
+            setTime(endDate - now);
         }, 1000);
 
         if (h == 0 && m == 0 && s == 0) {
@@ -19,7 +23,7 @@ const Timer = () => {
         return () => {
             clearInterval(timerId);
         };
-    }, [time]);
+    });
 
     if (h < 10) h = "0" + h;
     if (m < 10) m = "0" + m;
