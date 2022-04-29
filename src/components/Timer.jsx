a713c33d861ac66d1ba00ff6e282fcbad23a2151
Michiel Peeters <michiel.peeters@anvil.be>
2022-04-29 11:56:54 +0200

    Timer 1 hours +

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,39 +1,24 @@
 import { useEffect, useState } from "react";
+import moment from "moment";
 
 const Timer = () => {
-    const endDate = new Date("April 29, 2022 12:00:00");
-    const [time, setTime] = useState(endDate / 1000);
+    const currentDate = moment();
+    const future = moment("2022-04-29 12:00:00");
+    const timeLeft = moment(future.diff(currentDate)).format("HH:mm:ss");
 
-    let now = Date.now();
-    console.log(now);
-
-    let h = Math.floor(time / 360000);
-    let m = Math.floor((time % 360000) / 6000);
-    let s = Math.floor((time % 360000) % 6000);
+    const [time, setTime] = useState(timeLeft);
 
     useEffect(() => {
         const timerId = setInterval(() => {
-            setTime(endDate - now);
+            setTime(timeLeft);
         }, 1000);
-
-        if (h == 0 && m == 0 && s == 0) {
-            clearInterval(timerId);
-        }
 
         return () => {
             clearInterval(timerId);
         };
     });
 
-    if (h < 10) h = "0" + h;
-    if (m < 10) m = "0" + m;
-    if (s < 10) s = "0" + s;
-
-    return (
-        <div className="timer">
-            {h}:{m}:{s}
-        </div>
-    );
+    return <div className="timer">{time}</div>;
 };
 
 export default Timer;
