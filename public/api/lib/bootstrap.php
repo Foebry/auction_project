5247c3caf2ea0fbc57315699f5758f2e7b5921a0
Foebry <rain_fabry@hotmail.com>
2022-05-04 00:17:37 +0200

    Added missing requires

--- public/api/lib/bootstrap.php
+++ public/api/lib/bootstrap.php
@@ -3,8 +3,13 @@ require_once "access_control.php";
 
 require_once "models/Response.php";
 
+require_once "services/ResponseHandler.php";
+
+require_once "routes/Article.php";
 require_once "routes/Auction.php";
 require_once "routes/Authentication.php";
+require_once "routes/Bidding.php";
+require_once "routes/Category.php";
 require_once "routes/User.php";
 
 require_once "routing.php";
