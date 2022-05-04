e0337e903a2ba359104f921968c36ec6eff5330d
Michiel Peeters <michiel.peeters@anvil.be>
2022-05-04 14:35:11 +0200

    timer changes

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,7 +3,7 @@ import moment from "moment";
 
 const Timer = () => {
     const currentDate = moment().add(1, "hour");
-    const future = moment("2022-04-29 12:19:00");
+    const future = moment("2022-05-02 17:00:00");
     const timeLeft = moment(future.diff(currentDate)).format("HH:mm:ss");
 
     const [time, setTime] = useState(timeLeft);
