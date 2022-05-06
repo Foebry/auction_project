4be81de195574ad8303a5413cf08f38bec828e09
Foebry <rain_fabry@hotmail.com>
2022-05-06 09:00:45 +0200

    UseQuery + apiTypes

---
+++ src/hooks/useQuery.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const useQuery = (endpoint, options) => {
+
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    const [queryOptions, setQueryOptions] = useState(options);
+
+    const executeQuery = async (endpoint, opts={}) => {
+        setQueryOptions(opts);
+        console.log(opts);
+        setLoading(true);
+        setError(false);
+        try{
+            const { data } = await axios(endpoint);
+            setLoading(false);
+            setError(false);
+            setData(data);
+        }catch(error) {
+            setLoading(false);
+            setError(error);
+            setData([]);
+        }
+    }
+
+    useEffect(() => {
+        executeQuery(endpoint, options);
+    }, []);
+
+    const refetch = async (newOptions=null) => {
+        const opts = newOptions? {...queryOptions, ...newOptions} : queryOptions
+        await executeQuery(endpoint, opts);
+    }
+
+    return {loading, error, data, refetch}
+}  
---
+++ src/types/apiTypes.js
@@ -0,0 +1,14 @@
+export const endpoints = {
+    AUCTIONS: "http://localhost:8000/api/auctions",
+    AUCTION: "http://localhost:8000/api/auction/:id",
+    AUCTION_BIDDINGS: "http://localhost:8000/api/auction/:id/biddings",
+    ARTICLES: "http://localhost:8000/api/articles",
+    ARTICLE: "http://localhost:8000/api/article/:id",
+    BIDDINGS: "http://localhost:8000/api/biddings",
+    CATEGORIES: "http://localhost:8000/api/categories",
+    CATEGORY: "http://localhost:8000/api/category/:id",
+    USER: "http://localhost:8000/api/user/:id",
+    USER_ARTICLES: "http://localhost:8000/api/user/:id/articles",
+    REGISTER: "http://localhost:8000/api/register",
+    LOGIN: "http://localhost:800/api/login",
+};
