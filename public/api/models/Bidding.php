f088102a7636a32e969628136841e07bcb78ad8d
Michiel Peeters <michiel.peeters@anvil.be>
2022-05-08 14:31:00 +0200

    Creating classes of Database with constructor and settersÃ

--- public/api/models/Bidding.php
+++ public/api/models/Bidding.php
@@ -1,5 +1,77 @@
 <?php
 
-/**
- * @todo Create Bidding class
- */
+namespace models;
+
+class Bidding {
+
+    private $bid_id;
+
+    private $bid_user_id;
+
+    private $bid_auc_id;
+
+    private $bid_price;
+
+    /**
+     * @param $bid_id
+     * @param $bid_user_id
+     * @param $bid_auc_id
+     * @param $bid_price
+     */
+    public function __construct($bid_id, $bid_user_id, $bid_auc_id, $bid_price) {
+        $this->bid_id = $bid_id;
+        $this->bid_user_id = $bid_user_id;
+        $this->bid_auc_id = $bid_auc_id;
+        $this->bid_price = $bid_price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBidId() {
+        return $this->bid_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBidUserId() {
+        return $this->bid_user_id;
+    }
+
+    /**
+     * @param int $bid_user_id
+     */
+    public function setBidUserId($bid_user_id) {
+        $this->bid_user_id = $bid_user_id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBidAucId() {
+        return $this->bid_auc_id;
+    }
+
+    /**
+     * @param int $bid_auc_id
+     */
+    public function setBidAucId($bid_auc_id) {
+        $this->bid_auc_id = $bid_auc_id;
+    }
+
+    /**
+     * @return double
+     */
+    public function getBidPrice() {
+        return $this->bid_price;
+    }
+
+    /**
+     * @param double $bid_price
+     */
+    public function setBidPrice($bid_price) {
+        $this->bid_price = $bid_price;
+    }
+
+}
--- public/api/models/Container.php
+++ public/api/models/Container.php
@@ -1,72 +1,81 @@
 <?php
 
-    class Container{
-        private $articleHandler;
-        private $auctionHandler;
-        private $biddingHandler;
-        private $categoryHandler;
-        private $responseHandler;
-        private $userHandler;
-        private $dbManager;
+namespace models;
 
-        function __construct() {
-            print("created new container");
-            $this->articleHandler = null;
-            $this->auctionHandler = null;
-            $this->biddingHandler = null;
-            $this->categoryHandler = null;
-            $this->responseHandler = null;
-            $this->userHandler = null;
-            $this->dbManager = null;
-        }
+class Container {
 
-        function getArticleHandler(): ArticleHandler {
-            if($this->articleHandler === null){
-                $this->articleHandler = new ArticleHandler();
-            }
-            return $this->articleHandler;
-        }
+    private $articleHandler;
 
-        function getAuctionHandler(): AuctionHandler {
-            if ($this->auctionHandler === null ) {
-                $this->auctionHandler = new AuctionHandler();
-            }
-            return $this->auctionHandler;
-        }
+    private $auctionHandler;
 
-        function getBiddingHandler(): BiddingHandler {
-            if ( $this->biddingHandler === null ) {
-                $this->biddingHandler = new BiddingHandler();
-            }
-            return $this->biddingHandler;
-        }
+    private $biddingHandler;
 
-        function categoryHandler(): CategoryHandler {
-            if ( $this->categoryHandler === null ) {
-                $this->categoryHandler = new CategoryHandler();
-            }
-            return $this->categoryHandler;
-        }
+    private $categoryHandler;
 
-        function getResponseHandler(): ResponseHandler {
-            if ( $this->responseHandler === null ) {
-                $this->responseHandler = new ResponseHandler();
-            }
-            return $this->responseHandler;
-        }
+    private $responseHandler;
 
-        function getUserHandler(): UserHandler {
-            if ( $this->userHandler === null ) {
-                $this->userHandler = new UserHandler();
-            }
-            return $this->userHandler;
-        }
+    private $userHandler;
 
-        function getDbManager(): DbManager {
-            if ( $this->dbManager === null ) {
-                $this->dbManager = new DbManager();
-            }
-            return $this->dbManager;
-        }
-    
+    private $dbManager;
+
+    function __construct() {
+        print("created new container");
+        $this->articleHandler = NULL;
+        $this->auctionHandler = NULL;
+        $this->biddingHandler = NULL;
+        $this->categoryHandler = NULL;
+        $this->responseHandler = NULL;
+        $this->userHandler = NULL;
+        $this->dbManager = NULL;
     }
+
+    function getArticleHandler(): ArticleHandler {
+        if ($this->articleHandler === NULL) {
+            $this->articleHandler = new ArticleHandler();
+        }
+        return $this->articleHandler;
+    }
+
+    function getAuctionHandler(): AuctionHandler {
+        if ($this->auctionHandler === NULL) {
+            $this->auctionHandler = new AuctionHandler();
+        }
+        return $this->auctionHandler;
+    }
+
+    function getBiddingHandler(): BiddingHandler {
+        if ($this->biddingHandler === NULL) {
+            $this->biddingHandler = new BiddingHandler();
+        }
+        return $this->biddingHandler;
+    }
+
+    function categoryHandler(): CategoryHandler {
+        if ($this->categoryHandler === NULL) {
+            $this->categoryHandler = new CategoryHandler();
+        }
+        return $this->categoryHandler;
+    }
+
+    function getResponseHandler(): ResponseHandler {
+        if ($this->responseHandler === NULL) {
+            $this->responseHandler = new ResponseHandler();
+        }
+        return $this->responseHandler;
+    }
+
+    function getUserHandler(): UserHandler {
+        if ($this->userHandler === NULL) {
+            $this->userHandler = new UserHandler();
+        }
+        return $this->userHandler;
+    }
+
+    function getDbManager(): DbManager {
+        if ($this->dbManager === NULL) {
+            $this->dbManager = new DbManager();
+        }
+        return $this->dbManager;
+    }
+
+}
