b87d072656bb597ea7e85ca63794a86bed9ee04e
Tim Vercammen <83355491+timvercammen@users.noreply.github.com>
2022-05-08 10:50:49 +0200

    aanpassing comments Sander

--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -1,23 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import { Routes as views } from "../types/RouteTypes";
 import Index from "../views/Index";
-<<<<<<< HEAD
 import AuctionDetail from "./AuctionDetail";
-=======
 import Login from "./Login";
 import Register from "./Register";
->>>>>>> dev
 
 const Routing = () => {
     return (
         <Routes>
             <Route path={views.INDEX} element={<Index />} />
-<<<<<<< HEAD
             <Route path={views.AUCTION_DETAIL} element={<AuctionDetail />} />
-=======
             <Route exact path={views.LOGIN} element={<Login />} />
             <Route exact path={views.REGISTER} element={<Register />} />
->>>>>>> dev
         </Routes>
     );
 };
--- src/types/RouteTypes.js
+++ src/types/RouteTypes.js
@@ -1,10 +1,7 @@
 export const Routes = {
     INDEX: "/",
     CONTACT: "/contact",
-<<<<<<< HEAD
     AUCTION_DETAIL: "/detail",
-=======
     LOGIN: "/login",
     REGISTER: "/register",
->>>>>>> dev
 };
