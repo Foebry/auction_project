320fca2a2a77d98f0ed12941388d6b69dbe95341
Foebry <rain_fabry@hotmail.com>
2022-05-10 09:38:44 +0200

    login endpoint

--- public/api/routes/Authentication.php
+++ public/api/routes/Authentication.php
@@ -1,6 +1,32 @@
 <?php
 
-function handleAuthentication() {
-    print("POST authentication logic");
+use models\Response;
+
+/**
+ * handleAuthentication
+ *
+ * @param  \models\Container $container
+ * @param  string $payload
+ * @return \models\Response
+ */
+function handleAuthentication(\models\Container $container, string $payload): Response {
+    $payload = json_decode($payload, true);
+
+    // check if required fields are sent
+    if (
+        !in_array("usr_email", array_keys($payload)) ||
+        !in_array("usr_password", array_keys($payload))
+    ) {
+        $container->getResponseHandler()->badRequest();
+    }
+
+    $user = $container->getUserHandler()->getUserByEmail($payload["usr_email"], $container);
+
+    if ( !password_verify(($payload["usr_password"]), $user->getUsrPassword()) ){
+        $container->getResponseHandler()->unprocessableEntity("Invalid password");
+    }
+    // $container->getDbManager()->closeConnection();
+
+    return new Response(["usr_id"=>$user->getUsrId(), "usr_name"=>$user->getUsrName()]);
 
 }
