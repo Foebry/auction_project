99d8901337074ac11315e03a1cc1bf07ce822768
Foebry <rain_fabry@hotmail.com>
2022-05-17 22:20:50 +0200

    sending OK status back when POST request comes in

--- public/fs_sander/api/lib/access_control.php
+++ public/fs_sander/api/lib/access_control.php
@@ -2,3 +2,9 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS"){
+    header("HTTP/1.1 200 OK");
+    exit();
+}
