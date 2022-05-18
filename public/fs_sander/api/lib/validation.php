405e945df3d139e7cae8998be8789b33337940aa
Foebry <rain_fabry@hotmail.com>
2022-05-18 09:12:23 +0200

    validation of jwt secret

--- public/fs_sander/api/lib/validation.php
+++ public/fs_sander/api/lib/validation.php
@@ -19,3 +19,16 @@ use models\Container;
             }
         }
     }
+
+    function validateJWT(string $token): false | array {
+        [$header, $payload, $signature] = explode(".", $token);
+        
+        $header_decoded = json_decode(base64_decode($header), true);
+
+        // if signature is faked, return false
+        if (hash_hmac($header_decoded["alg"], "BOO", "$header.$payload") !== $signature)
+            return false;
+        
+        // if signature is correct, return the userInformation + tokenExp as array
+        return json_decode(base64_decode($payload), true);
+    }
