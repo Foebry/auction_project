942626efac6bd9ca1b655307928563cf79b78ccf
Michiel Peeters <michiel.peeters@anvil.be>
2022-05-22 17:31:15 +0200

    Article + Bidding Redux API

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,16 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import articleAPI from "./articleAPI";
 import auctionAPI from "./auctionAPI";
 import authenticationAPI from "./AuthenticationAPI";
+import biddingAPI from "./biddingAPI";
 
 const store = configureStore({
     reducer: combineReducers({
         [auctionAPI.reducerPath]: auctionAPI.reducer,
         [authenticationAPI.reducerPath]: authenticationAPI.reducer,
+        [articleAPI.reducerPath]: articleAPI.reducer,
+        [biddingAPI.reducerPath]: biddingAPI.reducer,
     }),
 });
 
