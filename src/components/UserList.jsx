61c35e22d10349f0a1065ff455d153a5ccf44a9e
Michiel Peeters <michiel.peeters@anvil.be>
2022-05-26 16:07:09 +0200

    userList design

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { MdDelete, MdModeEdit } from "react-icons/md";
+import { useGetUsersQuery } from "../data/userAPI";
 
 const UserList = () => {
+    const { data, isError, isLoading } = useGetUsersQuery(undefined, {
+        pollingInterval: 0,
+        refetchOnFocus: true,
+        refetchOnReconnect: true,
+    });
     return (
         <ul className="userlist">
             <li className="userlist__user">
                 <p>Naam</p>
-                <p>Email</p>
+                <p className="userlist__user__email">Email</p>
                 <p>Role</p>
                 <p>Buttons</p>
             </li>
+            {isError && <p>error..</p>}
+            {isLoading && <p>loading..</p>}
             <li className="userlist__user">
                 <p>Michiel Peeters</p>
-                <p>michiel1.peeters@gmail.com</p>
-                <p>Admin</p>
+                <p className="userlist__user__email">
+                    michiel1.peeters@gmail.com
+                </p>
+                <p className="userlist__user__role">Admin</p>
                 <div className="userlist__buttons">
                     <button className="userlist__buttons__button">
                         <MdModeEdit />
@@ -25,8 +35,8 @@ const UserList = () => {
             </li>
             <li className="userlist__user">
                 <p>Sander Fabry</p>
-                <p>rain_fabry@hotmail.com</p>
-                <p>Admin</p>
+                <p className="userlist__user__email">rain_fabry@hotmail.com</p>
+                <p className="userlist__user__role">Admin</p>
                 <div className="userlist__buttons">
                     <button className="userlist__buttons__button">
                         <MdModeEdit />
@@ -38,8 +48,8 @@ const UserList = () => {
             </li>
             <li className="userlist__user">
                 <p>Tim Vercammen</p>
-                <p>timvercammen@telenet.be</p>
-                <p>Admin</p>
+                <p className="userlist__user__email">timvercammen@telenet.be</p>
+                <p className="userlist__user__role">Admin</p>
                 <div className="userlist__buttons">
                     <button className="userlist__buttons__button">
                         <MdModeEdit />
